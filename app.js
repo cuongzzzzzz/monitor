569de4d5d3764d3f6af3b960101f1f25d73eaa93
kangzzz <kangzzz@hamsa.co>
2024-10-24 09:08:36 +0700

    b

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const client = require('prom-client');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Thiết lập metrics cho Prometheus
 const register = new client.Registry();
